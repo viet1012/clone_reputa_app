4287e0410ffc5d25c5ad300ae3b7b8f48e3fca42
Viet Ta <viettta1012@gmail.com>
2024-06-07 10:47:57 +0700

    ready to code

---
+++ lib/constants/constants.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class MyColors {
+  static const whiteColor = Color(0xffFFFFFF);
+  static const greenColor = Color(0xff1ED760);
+  static const primaryColor = Color(0xff054BAE);
+  static const blackColor = Color(0xff121212);
+  static const darGreyColor = Color(0xff282828);
+  static const lightGrey = Color(0xff777777);
+}
